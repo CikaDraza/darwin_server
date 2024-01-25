544020cfa618f6dee69f927eeeedd70c214eb9ac
CikaDraza <drazic.milan@gmail.com>
2024-01-25 21:16:47 +0100

    send correct model to db

--- controllers/cart_controller.js
+++ controllers/cart_controller.js
@@ -2,16 +2,17 @@ import expressAsyncHandler from "express-async-handler";
 import Cart from "../models/cart_model.js";
 
 export const addCart = expressAsyncHandler(async (req, res) => {
-    const { userId, productId, productImage, title, brand, articleCode, price } = req.body;
+    const { userId, items } = req.body;
+    console.log(req.body);
 
     try {
-        let cart = await Cart.findOne({ userId });
+        const cart = await Cart.findOne({ userId });
         console.log(cart);
         if (cart) {
-            cart.items.push({ productImage, productId, title, brand, articleCode, price });
+            cart.items.push(items);
             await cart.save();
         } else {
-            const newCart = await Cart.create({ userId, items: [{ productImage, productId, title, brand, articleCode, price }] });
+            const newCart = await Cart.create({ userId, items });
             await newCart.save();
         }
         res.status(200).json({ message: 'Product added to cart' });
@@ -39,12 +40,12 @@ export const getSingleCart = expressAsyncHandler(async (req, res) => {
 export const deleteProductCart = async (req, res) => {
   try {
 
-    const { userId, productId, productImage, title, brand, articleCode, price } = req.body;
+    const { userId, items } = req.body;
     const cart = await Cart.findOne({ userId });
     if (!cart) {
       return res.status(404).json({ message: "Cart not found" });
     }
-
+    const { productId } = items;
     const productIndex = cart.items.findIndex((item) => item._id.toString() === productId);
 
     if (productIndex === -1) {
